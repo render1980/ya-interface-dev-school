531349eb37edfbfde4a7ff5602547e4eb5eb11ce
fungus <fungus@MacBook-Pro-fungus.local>
2013-11-09 16:26:31 +0400

    add project with bem-style site

--- bem/pages/index/index.bemjson.js
+++
@@ -1,9 +0,0 @@
-({
-	block: 'b-page',
-	title: 'Главная',
-	head: [
-		{ elem: 'css', url: 'blocks/b-page/b-page.css' },
-		{ elem: 'js', url: 'blocks/b-page/b-page.bemhtml' }
-	],
-	content: 'WTF??!?!'	
-})
